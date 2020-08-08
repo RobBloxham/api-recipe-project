43246da4472088d0fde50e57465912af063ae4ff
Dan Boterashvili <37092027+dscription@users.noreply.github.com>
2020-08-07 22:21:24 -0400

    append expanded card

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-// Fake data until I can get Fetch working
-const recipes = [{"title":"Chicken Tortilla Wrap Sandwich","href":"http:\/\/www.recipezaar.com\/Chicken-Tortilla-Wrap-Sandwich-126712","ingredients":"cheddar cheese, salsa, chicken, flour tortillas, lettuce","thumbnail":"http:\/\/img.recipepuppy.com\/299609.jpg"},{"title":"Chicken Cordon Bleu Sandwich","href":"http:\/\/www.recipezaar.com\/Chicken-Cordon-Bleu-Sandwich-349780","ingredients":"chicken, french bread, ham, swiss cheese, vegetable oil","thumbnail":"http:\/\/img.recipepuppy.com\/295527.jpg"},{"title":"Chicken & Slaw Sandwich with Bell Pepper Relish ","href":"http:\/\/www.kraftfoods.com\/kf\/recipes\/chicken-slaw-sandwich-bell-110881.aspx","ingredients":"cheese spread, relish, sourdough bread, chicken, broccoli","thumbnail":"http:\/\/img.recipepuppy.com\/651857.jpg"},{"title":"Hot Chicken Sandwich Recipe","href":"http:\/\/cookeatshare.com\/recipes\/hot-chicken-sandwich-51902","ingredients":"chicken, eggs, olives, mushroom, mayonnaise","thumbnail":"http:\/\/img.recipepuppy.com\/794805.jpg"},{"title":"Parmesan Chicken Sandwich","href":"http:\/\/www.recipezaar.com\/Parmesan-Chicken-Sandwich-168023","ingredients":"black pepper, chicken, butter, oregano, bread crumbs, paprika, parmesan cheese, parsley, salt","thumbnail":"http:\/\/img.recipepuppy.com\/350088.jpg"},{"title":"Crispy Chicken Sandwich ","href":"http:\/\/www.kraftfoods.com\/kf\/recipes\/crispy-chicken-sandwich-55445.aspx","ingredients":"chicken, chicken, mozzarella cheese, french bread, dijon mustard, lettuce, tomato","thumbnail":"http:\/\/img.recipepuppy.com\/650315.jpg"}]
-
-
-
 
 // Arrays and Variables
 let mealChoice = [];
 let fridgeChoice = [];
 let proteinChoice;
+let recipeResults = null;
+let start;
+let endpoint;
+// let ready = true;
+
+
 
 // List of Food Items to be rendered as buttons
 const mealList = ['sandwich', 'soup', 'salad', 'baked', 'fried', 'slow cooked'];
@@ -16,46 +17,75 @@ const proteinList = ['chicken', 'fish', 'egg', 'tofu', 'beef'];
 
 // Cached Element References
 const body = document.body;
+const main = document.getElementById('main');
 const mealSelect = document.getElementById('select-meal');
 const fridgeItemsSelect = document.getElementById('select-fridge-items');
 const proteinSelect = document.getElementById('select-primary-protein');
+const searchRecipesButton = document.getElementById('search-recipes');
 const recipeSection = document.getElementById('returned-recipes');
-
 const recipeCards = document.getElementsByClassName('recipe-card');
-
 
 
 init();
 
 // Event Listeners
 body.addEventListener('click', e => handleChoice(e));
-// recipeSection.addEventListener('click', e => handleRecipeExpand(e));
-for (let i = 0; i < recipeCards.length; i++) {
-	recipeCards[i].addEventListener('click', e => console.log(e))
+searchRecipesButton.addEventListener('click', checkReady)
+
+
+
+function handleSelectedRecipe(e) {
+	console.log('recipe selected',e)
+}
+
+// Functions 
+function checkReady() {
+	if (proteinChoice) {
+		fetchData();
+		console.log('data fetched!');
+	}
+};
+
+// Create request string
+function createRequestString() {
+	if (proteinChoice) {
+		endpoint = `https://recipepuppyproxy.herokuapp.com/api/?i=${proteinChoice}&p=3`;
+		return endpoint;
+	}
 }
 
 // Fetch Data
 function fetchData() {
-	let url = 'https://recipepuppyproxy.herokuapp.com/api/?i=chicken&p=3';
+	createRequestString();
+	// let url = endpoint;
+	let url = "https://recipepuppyproxy.herokuapp.com/api/?i=chicken&q=sandwich&p=3";
 	
 	fetch(url)
 		.then((response) => {
 			if (response.status == 200) {
-				return response.json()
+				return response.json();
 			} else {
-				reject('server error')
+				reject('server error');
 			}
 		})
 		.then(data => {
 			let results = data.results;
+			recipeResults = results;
+			console.log('recipe results', recipeResults)
+			console.log('results',results);
 			createRecipeCard(results, recipeSection);
-		});
-}
-// Functions 
+			// console.log(recipeCards);
+			// console.log('new recipe array',Array.from(recipeCards))
+			for (let i=0; i<recipeCards.length;i++) {
+				recipeCards[i].addEventListener('click', e => {
+					// e.stopPropagation();
+					appendExpandedRecipeCard(results, e.target.id)
+				})
+			}
 
-// Expand recipe Selection
-function handleRecipeExpand(e) {
-	console.log(e);
+			return recipeResults;
+		});
+	
 }
 
 // Create Buttons
@@ -86,6 +116,27 @@ function appendRecipeCard(recipe, recipeContainer) {
 															<h2>${recipe.ingredients}</h2>
 														</div>`;
 	recipeContainer.appendChild(newRecipeCard);
+	
+}
+
+function appendExpandedRecipeCard(results, id) {
+	// take the recipeCard id (which is the title)
+	// match it to the data in 
+	
+	let selectedRecipe = results.filter(recipe => recipe.title.toLowerCase() === id);
+	let expandedRecipeCard = document.createElement("div");
+	expandedRecipeCard.setAttribute('class', 'card');
+	expandedRecipeCard.id ="selected-recipe";
+	expandedRecipeCard.innerHTML = `
+	<div class="card-body">
+				<img class="card-img-top"width="200" height="200" src=${selectedRecipe.thumbnail}>
+				<h1>You chose ${selectedRecipe.title}</h1>
+				<h2>${selectedRecipe.ingredients}</h2>
+				<div>
+					<h1></h1>
+				</div>
+			</div>`
+	main.appendChild(expandedRecipeCard);
 }
 
 function handleChoice(e) {
@@ -115,16 +166,14 @@ function handleChoice(e) {
 			proteinChoice = id;
 		}	
 	} 
-
-	// console.log('all lists', mealChoice, fridgeChoice, proteinChoice);
+	console.log(proteinChoice)
 }
+
+
 
 // Initialization Function
 function init() {
 	render();
-	fetchData();
-	// find a better home for this since you wont have this data until after sending a request once you get your data working.
-	// handleRecipes();
 }
 
 // Render Fuction
@@ -132,18 +181,18 @@ function render() {
 	createButtons(mealList, mealSelect);
 	createButtons(fridgeList, fridgeItemsSelect);
 	createButtons(proteinList,proteinSelect);
-	// createRecipeCard(recipes, recipeSection);
+	
 }
 
 
 
+// * Create expanded template recipe card.
 
-
+// Create request based off of user inputs.
 // * Send request to Recipe Puppy for Meal Type and Primary Protein.
 // // issue with request, CORs, I took a snapshot and will show it to instructors.
 //ask david why element.class = "doesnt work" when element.id = "does work" , instead I had to use setAtrribute('class', 'btn btn-primary')
 
-// * Create expanded template recipe card.
 // * Create Shopping list template.
 // * Listen for click event on rendered recipe card and jump to expanded recipe card.
 // * Listen to click event on Shopping List button to render Shopping List Template.
